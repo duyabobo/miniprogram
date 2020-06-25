5d625c6af9c2c9f0c70ad30b34b9b0bd76dd51d9
duyabo <duyabo@hundun.cn>
2020-06-25 10:13:12 +0800

    删除无用代码

--- config.js
+++ config.js
@@ -13,8 +13,7 @@ const guaninfo_url = '/guan_info'
 const guananswer_url = '/guan_answer'
 const guanpoint_url = '/guan_point'
 const evaluation_url = '/guan_evaluation' 
-const user_url = '/user'
-const suggestion_url = '/suggestion'
+const user_url = '/user' 
 
 const subscribe_offline_noti_tid = '0LeRGd69AHugmAOYDLHxut1DBZhpkZUdZb5f57DeD3g'
 
@@ -66,6 +65,5 @@ module.exports.guananswer_url = guananswer_url
 module.exports.evaluation_url = evaluation_url 
 module.exports.guanpoint_url = guanpoint_url
 module.exports.user_url = user_url
-module.exports.suggestion_url = suggestion_url
 module.exports.subscribe_offline_noti_tid = subscribe_offline_noti_tid
 module.exports.wxlogin = wxlogin
