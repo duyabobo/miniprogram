39d5deccb0f4568ee935b81e0c6ace677103ca2c
duyabo <duyabo@hundun.cn>
2022-02-13 11:54:22 +0800

    MONGTHPAY使用选择器

--- page/my_information/my_information.js
+++ page/my_information/my_information.js
@@ -54,6 +54,18 @@ Page({
     request.normalUpdateRequest(that, url, requestData)
   },
 
+  updateMonthPay: function(event) {
+    console.log('picker发送选择改变，携带值为', event.detail.value)
+    let that = this;
+    let url = config.HTTP_HOST_TEST + config.myselfUrl
+    let requestData = {
+      accessToken: app.globalData.accessToken,
+      opType: enumerate.MODEL_USER_OP_TYPE_MONTH_PAY,
+      value: event.detail.value
+    }
+    request.normalUpdateRequest(that, url, requestData)
+  },
+
   upsertMyself: function(event) {
     let that = this;
     wx.showActionSheet({
