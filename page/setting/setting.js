c5504f6a09a1dc1558427d48af2dabcc2944fedb
duyabo <duyabo@hundun.cn>
2022-03-03 09:05:59 +0800

    修改我的tab的css

--- page/setting/setting.js
+++ page/setting/setting.js
@@ -10,25 +10,6 @@ Page({
    */
   data: { },
 
-  logout: function () {
-    wx.request({
-      url: config.HTTP_HOST_TEST + config.loginUrl,
-      method: 'PUT',
-      data: {
-        accessToken: app.globalData.accessToken,
-      },
-      success(res) {
-        if (request.requestIsSuccess(res)) {
-          app.globalData.hasLogin = false
-          wx.reLaunch({
-            url: config.GUANGUAN_PAGE,
-          })
-        }
-      },
-      fail(res) {
-        request.logRequestErr("loginUrl err:", res)
-      }
-    })
-  },
+  
 
 })
