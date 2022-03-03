06cd3fb1ea4184c639d0b2e3dba78c894c6ab04d
duyabo <duyabo@hundun.cn>
2022-03-03 09:56:08 +0800

    logout

--- page/mine/mine.js
+++ page/mine/mine.js
@@ -96,27 +96,6 @@ Page({
     wxLogin.checkLoginBeforeJump(event.currentTarget.dataset.url, needLogin)
   },
 
-  logout: function () {
-    wx.request({
-      url: config.HTTP_HOST_TEST + config.loginUrl,
-      method: 'PUT',
-      data: {
-        accessToken: app.globalData.accessToken,
-      },
-      success(res) {
-        if (request.requestIsSuccess(res)) {
-          app.globalData.hasLogin = false
-          wx.reLaunch({
-            url: config.GUANGUAN_PAGE,
-          })
-        }
-      },
-      fail(res) {
-        request.logRequestErr("loginUrl err:", res)
-      }
-    })
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
--- page/setting/setting.js
+++ page/setting/setting.js
@@ -10,6 +10,25 @@ Page({
    */
   data: { },
 
-  
+  logout: function () {
+    wx.request({
+      url: config.HTTP_HOST_TEST + config.loginUrl,
+      method: 'PUT',
+      data: {
+        accessToken: app.globalData.accessToken,
+      },
+      success(res) {
+        if (request.requestIsSuccess(res)) {
+          app.globalData.hasLogin = false
+          wx.reLaunch({
+            url: config.GUANGUAN_PAGE,
+          })
+        }
+      },
+      fail(res) {
+        request.logRequestErr("loginUrl err:", res)
+      }
+    })
+  },
 
 })
